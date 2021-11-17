5faaceb3b72f210a61193ce4341fdd1f91da667a
Youggls <462046122@qq.com>
2021-11-17 16:39:11 +0800

    Finish device choose window.

--- ui/DeviceChooser.py
+++ ui/DeviceChooser.py
@@ -1,25 +1,51 @@
-from PyQt5.QtWidgets import QWidget, QLabel, QDialog, QListWidget, QListWidgetItem, QPushButton
+from PyQt5.QtCore import QRect
+from PyQt5.QtWidgets import QWidget, QLabel, QDialog, QListWidget, QListWidgetItem, QPushButton, QHBoxLayout, \
+    QTableWidget, QTableWidgetItem, QVBoxLayout, QMessageBox
 from tools import utils
 
 
 class DeviceChooser(QDialog):
     def __init__(self, main_window):
+        """
+        Initialization function
+        :param main_window: main windows object
+        """
         super().__init__()
         self.setWindowTitle('Choose Device')
-        self.device_list = utils.get_devices()
-        self.__init_ui()
+        self.device_dic = utils.get_devices()
+        self.device_id_list = [key for key in self.device_dic]
+        self.table = QTableWidget()
         self.main_window = main_window
+        self.__init_ui()
 
     def __init_ui(self):
-        list_widget = QListWidget(self)
-        for device in self.device_list:
-            btn = QPushButton(device)
-            btn.clicked.connect(self.list_item_listener)
-            item = QListWidgetItem(device)
-            list_widget.addItem(item)
-            list_widget.setItemWidget(item, btn)
+        self.setGeometry(self.main_window.main_window_rect)
+        self.resize(self.main_window.main_window_size)
+        self.setFixedSize(self.main_window.main_window_size)
+
+        total_container = QVBoxLayout()
+        confirm_button = QPushButton('Choose and Start Capture', self)
+        confirm_button.clicked.connect(self.start_button_listener)
+        total_container.addWidget(confirm_button)
+
+        self.table.setRowCount(len(self.device_dic))
+        self.table.setColumnCount(2)
+        self.table.setHorizontalHeaderLabels(['device name', 'device id'])
+        self.table.setVerticalHeaderLabels([str(i) for i in range(len(self.device_dic))])
+        for idx, device in enumerate(self.device_dic):
+            device_name_item = QTableWidgetItem(self.device_dic[device])
+            device_id_item = QTableWidgetItem(device)
+            self.table.setItem(idx, 0, device_name_item)
+            self.table.setItem(idx, 1, device_id_item)
+
+        total_container.addWidget(self.table)
+        self.setLayout(total_container)
+        self.adjustSize()
         self.show()
 
-    def list_item_listener(self):
-        self.main_window.set_device(self.sender().text())
+    def start_button_listener(self):
+        row = self.table.currentRow()
+        if row == -1:
+            QMessageBox.warning(self, 'Error!', 'Please choose legal row!')
+        self.main_window.set_device(self.device_id_list[row], self.device_dic[self.device_id_list[row]])
         self.close()
--- ui/MainWindow.py
+++ ui/MainWindow.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QWidget, QLabel, QPushButton, QLineEdit, QApplication
+from PyQt5.QtCore import QRect, QSize
+from PyQt5.QtWidgets import QWidget, QLabel, QPushButton, QLineEdit, QApplication, QVBoxLayout, QHBoxLayout
 from .DeviceChooser import DeviceChooser
 
 
@@ -9,7 +10,10 @@ class MainWindow(QWidget):
     def __init__(self, window_name, is_debug=True):
         super().__init__()
         # Get resolution of current monitor
-        self.device_id = QLabel('NULL', self)
+        self.device_name = 'Name: NULL'
+        self.device_id = 'ID: NULL'
+        self.device_id_label = QLabel(self.device_id, self)
+        self.device_name_label = QLabel(self.device_name, self)
         self.desktop = QApplication.desktop()
         self.screen_rect = self.desktop.screenGeometry()
         self.height = self.screen_rect.height()
@@ -21,9 +25,14 @@ class MainWindow(QWidget):
         # The start position of main window.
         self.window_pos_x = int(self.width / 2 - self.window_width / 2)
         self.window_pos_y = int(self.height / 2 - self.window_height / 2)
-
+        # Main windows rect
+        self.main_window_rect = QRect(self.window_pos_x, self.window_pos_y, self.window_width, self.window_height)
+        self.main_window_size = QSize(self.window_width, self.window_height)
+        # Set main window title
         self.setWindowTitle(window_name)
+        # Call init_ui to init window.
         self.init_ui()
+        # If is_debug is True, print basic information of main window.
         if is_debug:
             self.__print_info()
 
@@ -37,30 +46,49 @@ class MainWindow(QWidget):
         Init ui
         :return: None
         """
-        self.setGeometry(self.window_pos_x, self.window_pos_y, self.window_width, self.window_height)
+        # Set window size and position
+        self.setGeometry(self.main_window_rect)
         self.resize(self.window_width, self.window_height)
         self.setFixedSize(self.window_width, self.window_height)
-        # self.setWindowFlag(QtCore.Qt.WindowMinimizeButtonHint)
 
-        choose_device_btn = QPushButton('选择网卡', self)
-        choose_device_btn.move(20, 20)
-        choose_device_btn.clicked.connect(self.button_listener)
-        self.device_id.move(150, 20)
+        total_container = QVBoxLayout()
+        top_container = QHBoxLayout()
+        middle_container = QHBoxLayout()
+
+        choose_device_btn = QPushButton('Choose device', self)
+        # choose_device_btn.move(20, 20)
+        choose_device_btn.clicked.connect(self.choose_device_button_listener)
+        top_container.addWidget(choose_device_btn)
+        top_container.addWidget(self.device_id_label)
+        top_container.addWidget(self.device_name_label)
+        # self.device_id_label.move(220, 27)
 
         # Middle h-box, input capture duration time.
-        time_hint = QLabel('捕获时间 (s)', self)
-        time_hint.move(20, 60)
+        time_hint = QLabel('Capture duration (s)', self)
+        # time_hint.move(20, 60)
         time_input = QLineEdit(self)
-        time_input.move(150, 60)
+        # time_input.move(220, 57)
+
+        middle_container.addWidget(time_hint)
+        middle_container.addWidget(time_input)
+
+        total_container.addLayout(top_container)
+        total_container.addLayout(middle_container)
+
+        self.setLayout(total_container)
 
         self.show()
 
-    def button_listener(self):
-        sender = self.sender()
-        print('button')
+    def choose_device_button_listener(self):
         chooser = DeviceChooser(self)
         chooser.show()
         chooser.exec_()
 
-    def set_device(self, text):
-        self.device_id.setText(text)
+    def set_device(self, device_id, device_name):
+        self.device_id = device_id
+        self.device_id_label.setText('ID: ' + device_id)
+        self.device_name = device_name
+        self.device_name_label.setText('Name: ' + device_name)
+        self.device_id_label.adjustSize()
+        self.device_name_label.adjustSize()
+        self.show()
